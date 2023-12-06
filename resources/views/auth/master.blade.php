79570e230a94da2e4dacdc55448fdc4acdb13355
Donnydamara <119925897+Donnydamara@users.noreply.github.com>
2023-12-06 21:59:49 +0700

    halaman login

--- resources/views/auth/master.blade.php
+++ resources/views/auth/master.blade.php
@@ -11,9 +11,17 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Font Awesome Icons -->
-    <link rel="stylesheet" href="{{asset('css/fontawesome.min.css')}}">
+    <link rel="stylesheet" href="{{ asset('css/fontawesome.min.css') }}">
     <!-- Theme style -->
-    <link rel="stylesheet" href="{{asset('css/adminlte.min.css')}}">
+    {{-- <link rel="stylesheet" href="{{asset('css/adminlte.min.css')}}"> --}}
+    <link rel="stylesheet" type="text/css" href="css/util.css">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+    <link rel="icon" type="image/png" href="images/icons/favicon.ico" />
     <!-- Google Font: Source Sans Pro -->
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 
@@ -23,12 +31,13 @@
 <body class="hold-transition login-page">
     <div class="login-box">
         <div class="login-logo">
-            {{config('app.name')}}
+            {{ config('app.name') }}
         </div>
-        
+
         @yield('content')
     </div>
     <!-- /.login-box -->
+    <script src="js/main.js"></script>
 </body>
 
 </html>
